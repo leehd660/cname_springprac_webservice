f023364b024f5d8ba94265e7b45c75d578c44c67
leehd660 <86194199+leehd660@users.noreply.github.com>
2021-07-09 22:38:01 +0900

    21.07.09 testcode 공부하는 중

---
+++ src/main/java/com/cnameproject/springboot/Application.java
@@ -0,0 +1,11 @@
+package com.cnameproject.springboot; // 패키지명은 일반적으로 웹사이트 주소의 역순으로 함.
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // 스프링부트의 자동 설정, 스프링 Bean 읽기와 성성을 모두 자동으로 설정, 이것은 항상 프로젝트의 최상단에 위치해야함
+public class Application { //프로젝트의 메인클래스
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args); //SpringApplication.run으로 인해 내장 WAS(웹어플리케이션 서버)를 실행
+    }
+}
---
+++ src/main/java/web/HelloController.java
@@ -0,0 +1,13 @@
+package web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // 컨트롤러를 JSON을 반환하는 컨트롤러로 만들어 줌
+public class HelloController {
+
+    @GetMapping("/hello") //HTTP Method인 Get의 요청을 받을 수 있는 API를 만들어 줌
+    public String hello() { //이제 이 프로젝트는 /hello로 요청이 오면 문자열 hello를 반환하는 기능을 가지게 됨
+        return "hello";
+    }
+}
