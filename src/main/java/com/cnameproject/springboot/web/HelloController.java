36e0a4d7d2a928eef973bafe933ba622fa51d230
leehd660 <86194199+leehd660@users.noreply.github.com>
2021-07-10 15:10:01 +0900

    21.07.10 폴더 위치 바꿈 - 폴더 위치때문에 웹사이트 링크에서 사이트를 못찾았어서 404에러가 떴었음.

--- src/main/java/com/cnameproject/springboot/web/HelloController.java
+++ src/main/java/com/cnameproject/springboot/web/HelloController.java
@@ -1,4 +1,4 @@
-package web;
+package com.cnameproject.springboot.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/test/java/com/cnameproject/springboot/HelloControllerTest.java
+++ src/test/java/com/cnameproject/springboot/HelloControllerTest.java
@@ -7,23 +7,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import web.HelloController;
+import com.cnameproject.springboot.web.HelloController;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+//RunWith는 즉, 스프링부트 테스트와 Jnit 사이에 연결자 역할
+@RunWith(SpringRunner.class) //테스트를 진행할 때 Jnit에 내장된 실행자 외에 다른 실행자를 실행시킴. 여기서는 SpringRunner라는 스프링 실행자를 사용.
+@WebMvcTest(controllers = HelloController.class) //WebMvcTest는 web에 집중할 수 있는 어노테이션
 public class HelloControllerTest {
 
-    @Autowired
-    private MockMvc mvc;
+    @Autowired //스프링이 관리하는 빈(Bean)을 주입 받음
+    private MockMvc mvc; //웹API를 테스트할 때 사용함, 스프링 MVC테스트의 시작점, 이 클래스를 통해서 HTTP GET, POST 등에 대한 API테스트가 가능
 
     @Test
-    public void hello가_리턴된다() throws Exception {
+    public void helloReturn() throws Exception {
         String hello = "hello";
 
+        //mvc.perform(get("/hello")) : MockMvc를 통해 /hello 주소로 HTTP GET을 요청함.
+        //mvc.perform의 결과를 검증, HTTP Header의 Status를 검증, 흔히 200,404,500 등의 상태를 검증, 여기선 0K 즉 200인지 아니지를 검증
+        //Controller에서 "hello"를 리턴하기 때문에 이 값이 맞는지 검증함.
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
