82cdfac74a55fb4082ecc5eb11c06870a4258deb
leehd660 <86194199+leehd660@users.noreply.github.com>
2021-07-10 16:17:31 +0900

    21.07.10 롬복 테스트 코드가 계속 실패했는데 build.gradle에서 annotationProcessor와 compileOnly로 변경하니 실햄됨. 이유는 모름.

---
+++ src/test/java/com/cnameproject/springboot/dto/HelloResponseDtoTest.java
@@ -0,0 +1,22 @@
+package com.cnameproject.springboot.dto;
+
+import com.cnameproject.springboot.web.dto.HelloResponseDto;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void 롬복_기능_테스트() {
+        //give
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name); //assertThat : assertj라는 테스트 검증 라이브러리의 검증 메소드, 검증하고 싶은 대상을 메소드의 인자로 받음
+        assertThat(dto.getAmount()).isEqualTo(amount); //isEqualTo: assertj의 동등 비교 메소드, assertThat에 있는 값과 isEqualTo의 값을 비교해서 같을때만 성공
+    }
+}
