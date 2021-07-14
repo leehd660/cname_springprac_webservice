275ce86239ca96277c819acd230c90b23b0176f8
leehd660 <86194199+leehd660@users.noreply.github.com>
2021-07-14 17:21:25 +0900

    21.07.14 게시물 등록해서 디비에 넣는거 성공함!

--- src/main/java/com/cnameproject/springboot/Application.java
+++ src/main/java/com/cnameproject/springboot/Application.java
@@ -8,6 +8,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication // 스프링부트의 자동 설정, 스프링 Bean 읽기와 성성을 모두 자동으로 설정, 이것은 항상 프로젝트의 최상단에 위치해야함
 public class Application { //프로젝트의 메인클래스
     public static void main(String[] args) {
-        SpringApplication.run(Application.class,args); //SpringApplication.run으로 인해 내장 WAS(웹어플리케이션 서버)를 실행
+        SpringApplication.run(Application.class, args); //SpringApplication.run으로 인해 내장 WAS(웹어플리케이션 서버)를 실행
     }
 }
