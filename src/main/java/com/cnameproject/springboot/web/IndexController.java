e5ffccb9f1985fed4ea427446d4c18bcbd83f4e1
leehd660 <86194199+leehd660@users.noreply.github.com>
2021-07-19 22:36:06 +0900

    21.07.19 기본적인 구글로그인, 로그아웃, 회원가입, 권한관리 기능 모두 구현. 오류났던 상황 1 : application.properties에서 profiles를 틀려서 oauth설정값을 사용할 수 없었던 것. 2: mustache끼리의 변수?명이 서로 달라서 로그인을 했음에도 다음 화면으로 넘어가지 못하고 로그인창만 계속 떴었음(userName과 username)

--- src/main/java/com/cnameproject/springboot/web/IndexController.java
+++ src/main/java/com/cnameproject/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.cnameproject.springboot.web;
 
+import com.cnameproject.springboot.config.auth.dto.SessionUser;
 import com.cnameproject.springboot.service.posts.PostsService;
 import com.cnameproject.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -7,17 +8,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) { // Model : 서버 템플릿엔진에서 사용할 수 있는 객체를 저장할 수 있음
         //여기서는 postsService.findAllDesc()로 가져온 결과를 posts로 index.mustache에 전달
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user"); //앞서 작성된 CustomOAuth2UserService에서 로그인 성공시 세션에 SessionUser를 저장하도록 구성함.
+        //즉 로그인 성공 시 httpSession.getAttribute("user")에서 값을 가져올 수 있음.
+        if (user != null) {
+            //세션에 저장된 값이 있을 때만 model에 userName으로 등록함. 세션에 저장된 값이 없으면 model엔 아무런 값이 없는 상태니 로그인 버튼이 보이게 됨.
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
         //build.gradle에서 설치한 머스테치 스타터 덕분에 컨트롤러에서 문자열을 반환할 때 앞의 경로와 뒤의 파일 확장자는 자동으로 지정됨.
         //앞의 경로는 src/main/resources/templates로, 뒤의 파일 확장자는 .mustache가 붙음.
