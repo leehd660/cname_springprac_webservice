02a16e82b9607ee46fe619ab390b1a5aab860102
leehd660 <86194199+leehd660@users.noreply.github.com>
2021-07-20 15:43:02 +0900

    21.07.20 어노테이션 기반으로 코드 개선하기. IndexController에서 세션값을 가져오는 부분을 코드로 작성하였는데 이렇게 작성할 경우 다른 컨트롤러나 메소드에서 세션값이 필요하면 직접 세션값을 같은 코드로 가져와야함.(코드 반복) 따라서 메소드 인자로 세션값을 바로 받을 수 있도록 만듬.

---
+++ src/main/java/com/cnameproject/springboot/config/auth/WebConfig.java
@@ -0,0 +1,21 @@
+package com.cnameproject.springboot.config.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        //HandlerMethodArgumentResolver는 항상 WebMvcConfigurer의 addArgumentResolver()를 통해 추가해야함.
+        //다른 HandlerMethodArgumentResolver가 필요하다면 같은 방식으로 추가해주면 됨.
+        argumentResolvers.add(loginUserArgumentResolver);
+    }
+}
--- src/main/java/com/cnameproject/springboot/web/IndexController.java
+++ src/main/java/com/cnameproject/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.cnameproject.springboot.web;
 
+import com.cnameproject.springboot.config.auth.LoginUser;
 import com.cnameproject.springboot.config.auth.dto.SessionUser;
 import com.cnameproject.springboot.service.posts.PostsService;
 import com.cnameproject.springboot.web.dto.PostsResponseDto;
@@ -18,10 +19,11 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) { // Model : 서버 템플릿엔진에서 사용할 수 있는 객체를 저장할 수 있음
+    public String index(Model model, @LoginUser SessionUser user) { // Model : 서버 템플릿엔진에서 사용할 수 있는 객체를 저장할 수 있음
         //여기서는 postsService.findAllDesc()로 가져온 결과를 posts로 index.mustache에 전달
+        //@LoginUser를 어노테이션 처리해주면서 기존에 코드를 개선함. 이제 어느 컨트롤러든지 @LoginUser만 사용하면 세션정보 가져올 수 있음.
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user"); //앞서 작성된 CustomOAuth2UserService에서 로그인 성공시 세션에 SessionUser를 저장하도록 구성함.
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user"); //앞서 작성된 CustomOAuth2UserService에서 로그인 성공시 세션에 SessionUser를 저장하도록 구성함.
         //즉 로그인 성공 시 httpSession.getAttribute("user")에서 값을 가져올 수 있음.
         if (user != null) {
             //세션에 저장된 값이 있을 때만 model에 userName으로 등록함. 세션에 저장된 값이 없으면 model엔 아무런 값이 없는 상태니 로그인 버튼이 보이게 됨.
