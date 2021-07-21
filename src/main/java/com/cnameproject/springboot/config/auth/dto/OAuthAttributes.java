651a39493416d70ac0fe2fef668d3c93f41be44b
leehd660 <86194199+leehd660@users.noreply.github.com>
2021-07-21 14:37:29 +0900

    21.07.21 네이버 소셜 로그인 성공. 마지막에 invalid token response 오류가 있었는데, https를 잘못써서 일어난 오류였음. 구글과 마찬가지로 네이버 오픈 API에 프로젝트를 생성해서 연동하는 것. 구글 소셜과는 다른 점은 기준이 되는 user_name의 이름을 네이버에선 response로 해야한다는 점.(네이버가 성공했을 때 오픈API를 보면 알 수 있음)

--- src/main/java/com/cnameproject/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/cnameproject/springboot/config/auth/dto/OAuthAttributes.java
@@ -29,6 +29,11 @@ public class OAuthAttributes {
     //of() :OAuth2User에서 반환하는 사용자 정보는 Map이기 때문에 값 하나하나를 변환해야만 함.
     public static OAuthAttributes of (String registrationID, String userNameAttributeName,
                                       Map<String, Object> attributes) {
+        if ("naver".equals(registrationID)) {
+            //registrationID가 네이버인지 확인하는 코드
+            return ofNaver("id", attributes);
+        }
+
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -38,6 +43,18 @@ public class OAuthAttributes {
                 .email((String)attributes.get("email"))
                 .picture((String)attributes.get("picture"))
                 .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String,Object>)attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String)response.get("email"))
+                .picture((String)response.get("profile_image"))
+                .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
