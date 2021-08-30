f56163db352297eeb8987c8b8aa255771d0abd18
leehd660 <86194199+leehd660@users.noreply.github.com>
2021-08-30 16:51:33 +0900

    08.30 MySQL의 테이블 명이 틀려서 다시 고침.

--- src/main/java/com/cnameproject/springboot/domain/userinfo/UserInfoRepository.java
+++ src/main/java/com/cnameproject/springboot/domain/userinfo/UserInfoRepository.java
@@ -8,12 +8,12 @@ import java.util.List;
 
 public interface UserInfoRepository extends JpaRepository<User_info, Long> {
 
-    @Query("SELECT ui FROM User_info ui ORDER BY ui.id DESC")
+    @Query("SELECT ui FROM user_info ui ORDER BY ui.id DESC")
     List<User_info> findAllDesc();
 
-    @Query(value = "SELECT id FROM User_info ui where email = :email", nativeQuery = true)
+    @Query(value = "SELECT id FROM user_info ui where email = :email", nativeQuery = true)
     Long findIdByEmail(@Param("email") String email);
 
-    @Query(value = "SELECT * FROM User_info ui where ui.id = :id", nativeQuery = true)
+    @Query(value = "SELECT * FROM user_info ui where ui.id = :id", nativeQuery = true)
     User_info findInfoByID(@Param("id") Long id);
 }
