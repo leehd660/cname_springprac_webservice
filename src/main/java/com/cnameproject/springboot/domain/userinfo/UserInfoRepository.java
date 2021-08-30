cfbe727ccd57f15d8ffbc639b3fef79afe268555
leehd660 <86194199+leehd660@users.noreply.github.com>
2021-08-30 16:15:00 +0900

    08.30 UserInfoRepository 오타 수정

--- src/main/java/com/cnameproject/springboot/domain/userinfo/UserInfoRepository.java
+++ src/main/java/com/cnameproject/springboot/domain/userinfo/UserInfoRepository.java
@@ -8,10 +8,10 @@ import java.util.List;
 
 public interface UserInfoRepository extends JpaRepository<User_info, Long> {
 
-    @Query("SELECT ui FROM UserInfo ui ORDER BY ui.id DESC")
+    @Query("SELECT ui FROM User_info ui ORDER BY ui.id DESC")
     List<User_info> findAllDesc();
 
-    @Query(value = "SELECT id FROM UserInfo ui where email = :email", nativeQuery = true)
+    @Query(value = "SELECT id FROM User_info ui where email = :email", nativeQuery = true)
     Long findIdByEmail(@Param("email") String findEmail);
 
     @Query(value = "SELECT * FROM User_info ui where ui.id = :id", nativeQuery = true)
